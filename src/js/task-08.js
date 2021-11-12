a0b0376ec15c6c6833a186b3fa5eed4d913dda8e
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-11-12 22:18:47 +0200

    12

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -11,10 +11,13 @@
 //   </label>
 //   <button type="submit">Login</button>
 // </form>
-const formSub = document.querySelector('.login-form');
-formSub.addEventListener('imput',(event)=>{
-    console.log(`event`,event.target.value);
-})
+const formSub = (formSub) => document.querySelector(formSub);
+const classes = (classes) => document.querySelector(classes);
+console.log (classes);
+console.log (formSub);
+// formSub.addEventListener('submit',(event)=>{
+//     console.log(`event`,event.target.value);
+// })
 // Обработка отправки формы form.login-form должна быть по событию submit.
 
 // При отправке формы страница не должна перезагружаться.
