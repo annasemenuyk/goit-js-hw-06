b745cc5d8199c3ad27a93f831150a04a2fb6ae48
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-11-13 15:55:08 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -5,8 +5,16 @@
 //   <p>Background color: <span class="color">-</span></p>
 //   <button type="button" class="change-color">Change color</button>
 // </div>
-
+const backgroundSpanElem = document.querySelector ('.color');
+const btnElem = document.querySelector ('.change-color');
+const backgroundBodyElem = document.querySelector ('body');
+const colorBody = getRandomHexColor()
+btnElem.addEventListener ( `click`, cangeBackgroundColor);
+function cangeBackgroundColor () {
+  backgroundBodyElem.style.backgroundColor = colorBody;
+  backgroundSpanElem.textContent = colorBody;
 // Для генерации случайного цвета используй функцию getRandomHexColor.
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
 }
