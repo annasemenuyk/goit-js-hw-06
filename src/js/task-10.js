99d02d66508854951154f45059eb9a4634989174
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-11-13 17:13:01 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,40 +1,46 @@
 
-//  Напиши скрипт создания и очистки коллекции элементов. 
+// /* Напиши скрипт создания и очистки коллекции элементов. 
 // Пользователь вводит количество элементов в input и нажимает кнопку Создать, 
 // после чего рендерится коллекция.
- const create = document.querySelector('[data-create]');
- const destroy = document.querySelector('[data-destroy]');
- const boxes = document.getElementById("boxes");
-create.addEventListener ('click', getAmount);
-destroy.addEventListener (`click`, destroyBoxes);
+//  При нажатии на кнопку Очистить, коллекция элементов очищается.
+// Создай функцию createBoxes(amount), которая принимает 
+// 1 параметр amount - число. Функция создает столько div, 
+// сколько указано в amount и добавляет их в div#boxes.
+// Каждый созданный div:
+// Имеет случайный rgb цвет фона
+// Размеры самого первого div - 30px на 30px
+// Каждый следующий div после первого, 
+// должен быть шире и выше предыдущего на 10px
+// Создай функцию destroyBoxes(), которая очищает div#boxes 
+const refs = {
+  create : document.querySelector('[data-create]'),
+  destroy : document.querySelector('[data-destroy]'),
+  boxes : document.getElementById("boxes"),
+};
+refs.create.addEventListener("click", getAmount);
+refs.destroy.addEventListener("click", destroyBoxes);
+
 function getAmount() {
 const amount = +document.getElementById("controls").value;
 createBoxes(amount);
 }
-// Создай функцию createBoxes(amount), которая принимает 
-// 1 параметр amount - число. Функция создает столько div, 
-// сколько указано в amount и добавляет их в div#boxes.
 function createBoxes(amount) {
-// Размеры самого первого div - 30px на 30px
   const basicSize = 30;
   let fragment = document.createDocumentFragment();
-// Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
 for (let i = 0; i < amount; i++) {
   let size = basicSize + i + 10;
   let div = document.createElement('div');
   div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
   fragment.appendChild(div);
-};
-// Создай функцию destroyBoxes(), которая очищает div#boxes.
-boxes.appendChild(fragment);
 }
-//  При нажатии на кнопку Очистить, коллекция элементов очищается.
+
+refs.boxes.appendChild(fragment);
+}
+
 function destroyBoxes() {
-  boxes.innerHTML = "";
+  refs.boxes.innerHTML = "";
 }
-// Каждый созданный div:
-// Имеет случайный rgb цвет фона
+
 function random() {
 return Math.floor(Math.random() * 256);
 }
-
