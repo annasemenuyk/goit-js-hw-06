a7cfb2c4cf3ec817d3c5478ca286f137bf84dd11
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-11-13 15:23:08 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -11,6 +11,7 @@
 //   </label>
 //   <button type="submit">Login</button>
 // </form>
+// Обработка отправки формы form.login-form должна быть по событию submit.
 const form = document.querySelector('.login-form');
 // const classes = (classes) => document.querySelector(classes);
 // console.log (classes);
@@ -40,29 +41,31 @@ const form = document.querySelector('.login-form');
     
     function handleSubmit(event) {
       event.preventDefault();
+      // Для доступа к элементам формы используй свойство elements.
       const {
         elements: { email, password }
       } = event.currentTarget;
-    
+    // Если в форме есть незаполненные поля, выводи alert с предупреждением о том, 
+// что все поля должны быть заполнены.
       if (email.value === "" || password.value === "") {
-        return alert("Please fill in all the fields! Все поля должны быть заполнены");
+        return alert("Все поля должны быть заполнены!");
       }
     
       console.log(`Email: ${email.value}, Password: ${password.value}`);
+      // Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
       event.currentTarget.reset();
     }
     
-// Обработка отправки формы form.login-form должна быть по событию submit.
 
 
 
-// Если в форме есть незаполненные поля, выводи alert с предупреждением о том, 
-// что все поля должны быть заполнены.
+
+
 
 
 
 // Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, 
 // где имя поля будет именем свойства, а значение поля - значением свойства. 
-// Для доступа к элементам формы используй свойство elements.
 
-// Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+
+
